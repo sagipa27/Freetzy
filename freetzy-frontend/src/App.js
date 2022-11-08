908171eabe5e94a80227231d9450115456190d1a
Cristóbal García <cristobalcabrerag@hotmail.com>
2022-11-08 16:48:41 -0500

    NavBar implementada sin navegación

--- freetzy-frontend/src/App.js
+++ freetzy-frontend/src/App.js
@@ -1,52 +1,10 @@
 import "./App.css";
-
-//Import wouter
-import { Link, Route } from "wouter";
-
-//Import páginas
-import LoginScreen from "./pages/LoginScreen";
-import RegisterScreen from "./pages/RegisterScreen";
-import ResetPasswordScreen from "./pages/ResetPasswordScreen";
-import ForgotPasswordScreen from "./pages/ForgotPasswordScreen";
-import PrivateScreen from "./pages/PrivateScreen";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <div>
-        <Link href="/login">
-          <a href="/login">Iniciar sesión </a>
-        </Link>
-        <Route path="/login" component={LoginScreen} />
-      </div>
-
-      <div>
-        <Link href="/forgotpassword">
-          <a href="/forgotpassword">Contraseña olvidada</a>
-        </Link>
-        <Route path="/forgotpassword" component={ForgotPasswordScreen} />
-      </div>
-
-      <div>
-        <Link href="/register">
-          <a href="/register">Registrarse</a>
-        </Link>
-        <Route path="/register" component={RegisterScreen} />
-      </div>
-
-      <div>
-        <Link href="/Private">
-          <a href="/Private">Private</a>
-        </Link>
-        <Route path="/Private" component={PrivateScreen} />
-      </div>
-
-      <div>
-        <Link href="/resetpassword">
-          <a href="/resetpassword">Restablecer contraseña</a>
-        </Link>
-        <Route path="/resetpassword" component={ResetPasswordScreen} />
-      </div>
+      <NavBar />
     </>
   );
 }
